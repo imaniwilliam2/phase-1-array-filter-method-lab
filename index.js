afd1e9aad85ce3ad2212334c1887da5f550559b8
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-01-31 16:20:26 -0500

    complete lab'

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+function findMatching(driverNamesArray, stringToMatch){
+    return driverNamesArray.filter(driverName => {
+        return driverName.toLowerCase() === stringToMatch.toLowerCase()
+    })
+
+}
+
+function fuzzyMatch(driversNamesArray, stringToMatch){
+    return driversNamesArray.filter((driverName) => {
+        return driverName.slice(0, stringToMatch.length) === stringToMatch})
+}
+
+function matchName(driverNameObject, stringToMatch){
+    return driverNameObject.filter(driverObject => {
+        return driverObject.name.toLowerCase() === stringToMatch.toLowerCase()
+    })
+
+}
+
+
+
+
